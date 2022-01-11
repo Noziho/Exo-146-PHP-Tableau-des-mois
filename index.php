df9d5462f2c835e445b45527b0883fe64b21035d
Noah2211 <noah.decroix3@gmail.com>
2022-01-11 14:53:42 +0100

    exercice 4/5/6/7/8/9/10 all done

--- index.php
+++ index.php
@@ -15,9 +15,27 @@ $tab = [
 ];
 
 echo $tab[2]."<br>";
-echo $tab[4]."<br>";
+echo $tab[4]."<br><br>";
 $tab[7]= "août";
 
 $tabDepartment = [
-
+    "59" => "Nord",
 ];
+
+echo $tabDepartment["59"] ."<br><br>";
+$tabDepartment["51"] = "Reims";
+
+foreach ($tab as $values) {
+    echo $values ."<br>";
+}
+
+echo "<br><br>";
+foreach ($tabDepartment as $values) {
+    echo $values ."<br>";
+}
+
+foreach ($tabDepartment as $key => $value) {
+    echo "Le département $value a le numéro $key" ."<br>";
+}
+
+
