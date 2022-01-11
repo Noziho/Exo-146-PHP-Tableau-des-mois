facd335850b7c8a3f78f2cde11253a8944f03a63
Noah2211 <noah.decroix3@gmail.com>
2022-01-11 13:35:18 +0100

    exercice 1/2/3/4

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$tab = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "aout",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre"
+];
+
+echo $tab[2]."<br>";
+echo $tab[4]."<br>";
+$tab[7]= "août";
+
+$tabDepartment = [
+
+];
